de6761fb26d6d13a0c0ebb1457077f43c3a14630
WataNekko <88185666+WataNekko@users.noreply.github.com>
2025-10-03 17:04:07 +0200

    Change BleMidiPacket to min capacity

--- src/trouble_midi.rs
+++ src/trouble_midi.rs
@@ -6,7 +6,7 @@ use trouble_host::{prelude::*, types::gatt_traits::FromGattError};
 #[gatt_service(uuid = "03B80E5A-EDE8-4B33-A751-6CE34EC4C700")]
 pub struct MidiService {
     #[characteristic(uuid = "7772E5DB-3868-4112-A1A9-F2669D106BF3", read, write_without_response, notify, value = MidiMessage::Reset.into())]
-    pub midi_event: BleMidiPacket<251>,
+    pub midi_event: BleMidiPacket<5>,
 }
 
 pub trait AsTimestamp {
@@ -31,8 +31,8 @@ impl AsTimestamp for Instant {
     }
 }
 
-pub struct BleMidiPacket<const SIZE: usize> {
-    buffer: [u8; SIZE],
+pub struct BleMidiPacket<const CAP: usize> {
+    buffer: [u8; CAP],
     len: usize,
 }
 
@@ -40,19 +40,21 @@ fn is_system_msg_status_byte(status: u8) -> bool {
     status & 0xF0 == 0xF0
 }
 
-impl<const SIZE: usize> BleMidiPacket<SIZE> {
+impl<const CAP: usize> BleMidiPacket<CAP> {
     const MIN_SIZE: usize = 3; // Header + Timestamp + Single MIDI status byte
-    const _OK: () = assert!(SIZE >= Self::MIN_SIZE);
+    const MIN_CAP: usize = 5; // Header + Timestamp + MIDI status byte + 2 Data bytes
 
     pub fn add_timestamped(
         timestamp: impl AsTimestamp,
         msg: MidiMessage,
-    ) -> BleMidiPacketBuilder<SIZE> {
+    ) -> BleMidiPacketBuilder<CAP> {
+        const { assert!(CAP >= Self::MIN_CAP) };
+
         let millis = timestamp.as_timestamp();
         let header = 0x80 | ((millis >> 7) as u8 & 0x3F);
         let timestamp = 0x80 | (millis as u8 & 0x7F);
 
-        let mut buffer = [0; SIZE];
+        let mut buffer = [0; CAP];
         buffer[0] = header;
         buffer[1] = timestamp;
 
@@ -76,33 +78,33 @@ impl<const SIZE: usize> BleMidiPacket<SIZE> {
     }
 }
 
-impl<Ts: AsTimestamp, const SIZE: usize> From<(Ts, MidiMessage)> for BleMidiPacket<SIZE> {
+impl<Ts: AsTimestamp, const CAP: usize> From<(Ts, MidiMessage)> for BleMidiPacket<CAP> {
     fn from((timestamp, msg): (Ts, MidiMessage)) -> Self {
         Self::add_timestamped(timestamp, msg).build()
     }
 }
 
-impl<const SIZE: usize> From<MidiMessage> for BleMidiPacket<SIZE> {
+impl<const CAP: usize> From<MidiMessage> for BleMidiPacket<CAP> {
     fn from(msg: MidiMessage) -> Self {
         Self::add_timestamped(Instant::now(), msg).build()
     }
 }
 
-impl<const SIZE: usize> AsGatt for BleMidiPacket<SIZE> {
+impl<const CAP: usize> AsGatt for BleMidiPacket<CAP> {
     const MIN_SIZE: usize = Self::MIN_SIZE;
-    const MAX_SIZE: usize = SIZE;
+    const MAX_SIZE: usize = CAP;
 
     fn as_gatt(&self) -> &[u8] {
         &self.buffer[..self.len]
     }
 }
 
-impl<const SIZE: usize> FromGatt for BleMidiPacket<SIZE> {
+impl<const CAP: usize> FromGatt for BleMidiPacket<CAP> {
     fn from_gatt(data: &[u8]) -> Result<Self, FromGattError> {
         if data.len() < Self::MIN_SIZE || data.len() > Self::MAX_SIZE {
             Err(FromGattError::InvalidLength)
         } else {
-            let mut buffer = [0; SIZE];
+            let mut buffer = [0; CAP];
             let len = data.len();
             buffer[..len].copy_from_slice(data);
             // Copy data directly without parsing. Provide some way to get the data from the packet
@@ -114,14 +116,14 @@ impl<const SIZE: usize> FromGatt for BleMidiPacket<SIZE> {
 }
 
 #[allow(unused)]
-pub struct BleMidiPacketBuilder<const SIZE: usize> {
-    packet: BleMidiPacket<SIZE>,
+pub struct BleMidiPacketBuilder<const CAP: usize> {
+    packet: BleMidiPacket<CAP>,
     running_status: Option<u8>,
     timestamp_byte: u8,
 }
 
-impl<const SIZE: usize> BleMidiPacketBuilder<SIZE> {
-    pub fn build(self) -> BleMidiPacket<SIZE> {
+impl<const CAP: usize> BleMidiPacketBuilder<CAP> {
+    pub fn build(self) -> BleMidiPacket<CAP> {
         self.packet
     }
 
